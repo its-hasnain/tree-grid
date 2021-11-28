b3abb21cb6710f5a52dc72e6d228fde2f916d45b
its-hasnain <itshasnain06@gmail.com>
2021-11-28 21:11:57 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { sampleData } from './datasource';
+import { ToolbarItems } from '@syncfusion/ej2-angular-treegrid';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'tree-grid';
+
+  public data: Object[] = [];
+  public editSettings!: Object;
+    public contextMenuItems: Object[] = [];
+    public columnColumnChooser: Object[] = [];
+  toolbar: string[] = [];
+
+
+    ngOnInit(): void {
+        this.data = sampleData;
+        this.editSettings = {allowEditing: true, allowAdding: true, allowDeleting: true, mode:"Row"};
+        this.contextMenuItems = ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending','Edit', 'Delete', 'Save', 'Cancel', 'PdfExport', 'ExcelExport', 'CsvExport'];
+        this.toolbar= ['ColumnChooser']; 
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageService,  FilterService,EditService, ColumnChooserService,ToolbarService } from '@syncfusion/ej2-angular-treegrid';
+import { SortService, ResizeService, ExcelExportService, PdfExportService, ContextMenuService } from '@syncfusion/ej2-angular-treegrid';
+import {ButtonModule} from '@syncfusion/ej2-angular-buttons';
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeGridModule,
+    ButtonModule,
+    DropDownListAllModule
   ],
-  providers: [],
+  providers: [
+    PageService,
+    SortService,
+    FilterService,
+    EditService,
+    ToolbarService,
+    ResizeService,
+    ExcelExportService,
+    PdfExportService,
+    ContextMenuService,
+    ColumnChooserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
